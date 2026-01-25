335b54bbff5c0d665de5e9c3240f23062ab263c0
Kunal Singh <kunalindia59@gmail.com>
2026-01-25 23:56:24 +0530

    Responsiveness issue fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,22 @@
 
 
 import './App.css'
+import Features from './components/Features'
 import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
 
 function App() {
 
   return (
-    <>
-      <div className='w-screen h-screen flex items-center justify-center'>
+    <div className='space-y-20 overflow-x-hidden'>
+      <div className='w-full flex flex-col items-center'>
         <Navbar />
         <HeroSection />
       </div>
-
-    </>
+      <div className='w-full flex flex-col items-center'>
+        <Features />
+      </div>
+    </div>
   )
 }
 
