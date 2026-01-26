eee8e955dab4923c54ba851f19c2ff043989eb15
Kunal Singh <kunalindia59@gmail.com>
2026-01-26 20:37:26 +0530

    Using protected routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,25 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import LandingPage from './pages/LandingPage'
+import Dashboard from './pages/Dashboard'
+import SignInPage from './pages/SignInPage'
+import SignUpPage from './pages/SignUpPage'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<LandingPage />} />
+      <Route path='/signin' element={<SignInPage />} />
+      <Route path='/signup' element={<SignUpPage />} />
+      <Route path='/dashboard'
+        element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>} />
+
+
     </Routes>
   )
 }
