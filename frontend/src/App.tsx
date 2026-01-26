2fa55e75573adde459c000dc01b16b9cf37e6244
Kunal Singh <kunalindia59@gmail.com>
2026-01-26 13:18:34 +0530

    Adding routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,15 @@
 
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import CTA from './components/CTA'
-import Features from './components/Features'
-import Footer from './components/Footer'
-import Founder from './components/Founder'
-import HeroSection from './components/HeroSection'
-import Integrations from './components/Integrations'
-import Navbar from './components/Navbar'
+import LandingPage from './pages/LandingPage'
 
 function App() {
 
   return (
-    <div className='space-y-20 overflow-x-hidden'>
-      <div className='w-full flex flex-col items-center'>
-        <Navbar />
-        <HeroSection />
-      </div>
-      <div className='w-full flex flex-col items-center gap-12'>
-        <Features />
-        <Integrations />
-        <Founder />
-        <CTA />
-        <Footer />
-      </div>
-    </div>
+    <Routes>
+      <Route path='/' element={<LandingPage />} />
+    </Routes>
   )
 }
 
