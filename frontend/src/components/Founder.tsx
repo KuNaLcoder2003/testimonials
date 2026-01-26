0efffd6842d699af8bf7acd29409c95ae4b1add5
Kunal Singh <kunalindia59@gmail.com>
2026-01-26 13:13:00 +0530

    Founder section

---
+++ frontend/src/components/Founder.tsx
@@ -0,0 +1,10 @@
+const Founder = () => {
+    return (
+        <div className="w-[75%] mx-auto h-[900px] mt-10">
+            <script type="text/javascript" src="https://testimonial.to/js/iframeResizer.min.js"></script>
+            <iframe id="testimonialto-embed-text--OjoCMzPf4f_b0MEa8SB" src="https://embed-v2.testimonial.to/text/-OjoCMzPf4f_b0MEa8SB" frameBorder="0" scrolling="no" height="100%" width="100%"></iframe>
+        </div>
+    )
+}
+
+export default Founder
