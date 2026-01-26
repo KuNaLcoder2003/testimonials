39e2276f1f8f7cbcfb66725cd6f9acc26e757723
Kunal Singh <kunalindia59@gmail.com>
2026-01-26 20:30:03 +0530

    Page for signin

---
+++ frontend/src/pages/SignInPage.tsx
@@ -0,0 +1,12 @@
+import type React from "react";
+import SignIn from "../components/Signin";
+
+const SignInPage: React.FC = () => {
+    return (
+        <div className="">
+            <SignIn />
+        </div>
+    )
+}
+
+export default SignInPage;
