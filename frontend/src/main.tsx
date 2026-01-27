332ebfcd50b10624360b3932515ac4830b806927
Kunal Singh <kunalindia59@gmail.com>
2026-01-27 23:27:30 +0530

    Adding toasts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,14 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.tsx'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')!).render(
 
 
   <BrowserRouter>
     <AuthProvider>
+      <Toaster />
       <App />
     </AuthProvider>
   </BrowserRouter>
