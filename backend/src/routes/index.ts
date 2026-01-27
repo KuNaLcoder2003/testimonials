34ffa6922ce1bffa09f057c046dda35c7875bb95
Kunal Singh <kunalindia59@gmail.com>
2026-01-27 23:31:16 +0530

    Adding spaceRouter

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,10 @@
 import express from "express"
 import userRouter from "./users/user.js";
+import spaceRouter from "./spaces/space.js";
 
 const sub_router = express.Router()
 
 sub_router.use('/user', userRouter)
+sub_router.use('/space', spaceRouter)
+
 export default sub_router;
