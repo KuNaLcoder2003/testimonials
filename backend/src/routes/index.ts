75a11c9d1ba882abebcdc1404f55a3e7ca9bd930
Kunal Singh <kunalindia59@gmail.com>
2026-01-28 23:30:32 +0530

    Adding testimonials router to subRouter

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from "express"
 import userRouter from "./users/user.js";
 import spaceRouter from "./spaces/space.js";
+import testiMonialRouter from "./testimonials/testimonials.js";
 
 const sub_router = express.Router()
 
 sub_router.use('/user', userRouter)
 sub_router.use('/space', spaceRouter)
+sub_router.use('/testimonial', testiMonialRouter)
 
 export default sub_router;
